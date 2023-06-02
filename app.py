21a40685cd1367d315903787392b719272d62a8f
Ziam-am <127385299+Ziam-am@users.noreply.github.com>
2023-06-02 11:50:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 
-from flask import Flask,render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+from tensorflow.keras.models import load_model
+from flask_ngrok import run_with_ngrok
+
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
-from joblib import load
+import re
+import pickle
+import nltk
+import joblib
+
+from process import preparation, generate_response
+
+preparation()
 
 # =[Variabel Global]=============================
 
@@ -17,55 +27,26 @@ model = None
 def beranda():
     return render_template('index.html')
 
-# [Routing untuk API]	
-@app.route("/api/deteksi",methods=['POST'])
-def apiDeteksi():
-	# Nilai default untuk variabel input atau features (X) ke model
-	input_sepal_length = 5.1
-	input_sepal_width  = 3.5
-	input_petal_length = 1.4
-	input_petal_width  = 0.2
-	
-	if request.method=='POST':
-		# Set nilai untuk variabel input atau features (X) berdasarkan input dari pengguna
-		input_sepal_length = float(request.form['sepal_length'])
-		input_sepal_width  = float(request.form['sepal_width'])
-		input_petal_length = float(request.form['petal_length'])
-		input_petal_width  = float(request.form['petal_width'])
-		
-		# Prediksi kelas atau spesies bunga iris berdasarkan data pengukuran yg diberikan pengguna
-		df_test = pd.DataFrame(data={
-			"SepalLengthCm" : [input_sepal_length],
-			"SepalWidthCm"  : [input_sepal_width],
-			"PetalLengthCm" : [input_petal_length],
-			"PetalWidthCm"  : [input_petal_width]
-		})
+# [Routing untuk Halaman Utama atau Home]	
+@app.route("/chatbot")
+def chatbot():
+    return render_template('chatbot.html')
 
-		hasil_prediksi = model.predict(df_test[0:1])[0]
-
-		# Set Path untuk gambar hasil prediksi
-		if hasil_prediksi == 'Iris-setosa':
-			gambar_prediksi = '/static/images/iris_setosa.jpg'
-		elif hasil_prediksi == 'Iris-versicolor':
-			gambar_prediksi = '/static/images/iris_versicolor.jpg'
-		else:
-			gambar_prediksi = '/static/images/iris_virginica.jpg'
-		
-		# Return hasil prediksi dengan format JSON
-		return jsonify({
-			"prediksi": hasil_prediksi,
-			"gambar_prediksi" : gambar_prediksi
-		})
-
-# =[Main]========================================
+# Routing for API response chatbot
+@app.route("/get")
+def get_bot_response():
+    user_input = str(request.args.get('msg'))
+    result = generate_response(user_input)
+    return result
 
 if __name__ == '__main__':
 	
 	# Load model yang telah ditraining
-	model = load('model_iris_dt.model')
+	#model = load('model_iris_dt.model')
 
-	# Run Flask di localhost 
-	app.run(host="localhost", port=5000, debug=True)
+	# Run Flask di google colab 
+	run_with_ngrok(app)
+	app.run()
 	
 	
 
